cf91c66c6805613c206c0aa18ea47832aa0f83d4
nongatu <nongatubio@gmail.com>
2026-05-19 01:24:14 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://cvcaednwgxfmtjdwktlj.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_1tfjLV9OndRtq25dd_jE-g_8y-QMeLk'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
